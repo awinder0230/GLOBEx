a9cb08839bc333633143a745c0f7a8c12050803f
awinder0230 <awinder0230@gmail.com>
2017-01-23 00:56:51 +0800

    HomePage Slider and Welcome Page Bug Fixed

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,9 +1,32 @@
 import React, { Component } from 'react';
+import 'isomorphic-fetch';
+import HomePageSlider from './HomePageSlider.js';
 
 class HomePage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      best_sellers_articles: [],
+    };
+  }
+
+/*  componentDidMount() {
+    // fetch best sellers for HomePageSlider
+    fetch('/api/articles/query?num=3')
+    .then(res => res.json())
+    .then(json => { 
+      this.setState({ best_sellers_articles: json });
+    });
+  }*/
+
   render() {
     return (
-      <div>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12">
+            <HomePageSlider/>
+          </div>
+        </div>
       </div>
     );
   }
--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -8,7 +8,7 @@ class WelcomePage extends Component {
       <div className="container-fluid">
         <div className="video-background">
           <div className="video-foreground">
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/PZ4pctQMdg4?autoplay=1&loop=1&playlist=PZ4pctQMdg4" frameborder="0" allowfullscreen></iframe>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/PZ4pctQMdg4?autoplay=1&loop=1&playlist=PZ4pctQMdg4" frameBorder="0" allowFullScreen></iframe>
           </div>
         </div>
       </div>
