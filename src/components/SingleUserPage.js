476d75435deb88e6f57d079035b362289775e595
dingcsw <b03901119@ntu.edu.tw>
2017-02-15 03:54:43 +0800

    add feature: new article

--- src/components/SingleUserPage.js
+++ src/components/SingleUserPage.js
@@ -11,6 +11,8 @@ class SingleUserPage extends Component {
       },
       articles: []
     };
+
+    this.newArticle = this.newArticle.bind(this);
   }
 
   componentDidMount() {
@@ -22,6 +24,29 @@ class SingleUserPage extends Component {
           .then(res => { return res.json(); })
           .then(json => { console.log('articles', json) ; this.setState({ articles: json }); });
       });
+  }
+
+  newArticle() {
+    /*
+    fetch(`api/articles/${id}`, {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        userId: this.state.user.account,
+        id: state.id,
+        title: this.newTitle.value,
+        author: this.state.user.name,
+        tags: [],
+        location: this.newLocation.value,
+        content: this.newContent.value,
+        popularity: this.newPopularity.value,
+        article: null
+      }),
+    });
+    */
   }
 
   render() {
@@ -38,9 +63,16 @@ class SingleUserPage extends Component {
             </div>
           </div>
         </div>
-        <div className="row">
-          {this.state.articles.map((article, index) => 
-            { return ( 
+        <button
+          type="button"
+          className="btn btn-gourmet btn-pull-right"
+          data-toggle="modal"
+          data-target="#modal-new-article">
+          New Article
+        </button>
+        <div className="row margin-top-50">
+          {this.state.articles.map((article, index) =>
+            { return (
               <SingleUserPageArticle key={index +1 }
                 title={article.title}
                 href={`#/articles/${article._id}`}
@@ -48,6 +80,81 @@ class SingleUserPage extends Component {
                 content={article.content}
               />);
             })}
+        </div>
+
+        <div className="modal fade" id="modal-new-article" tabIndex="-1" role="dialog">
+          <div className="modal-dialog" role="document">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h3>New Article</h3>
+              </div>
+              <div className="modal-body">
+                <div className="container-fluid">
+                  <form role="form">
+                    <div className="form-group row">
+                      <label className="col-form-label col-md-2 col-xs-2" htmlFor="inputTitle">
+                        Title
+                      </label>
+                      <div className="col-md-10 col-xs-10">
+                        <input
+                          className="form-control"
+                          id="inputTitle"
+                          type="text"
+                          ref={(input) => { this.newTitle = input; }}
+                        />
+                      </div>
+                    </div>
+                    <div className="form-group row">
+                      <label className="col-form-label col-md-2 col-xs-2" htmlFor="inputLocation">
+                        Location
+                      </label>
+                      <div className="col-md-10 col-xs-10">
+                        <input
+                          className="form-control"
+                          id="inputLocation"
+                          type="text"
+                          ref={(input) => { this.newLocation = input; }}
+                        />
+                      </div>
+                    </div>
+                    <div className="form-group row">
+                      <label className="col-form-label col-md-2 col-xs-2" htmlFor="inputPopularity">
+                        Popularity
+                      </label>
+                      <div className="col-md-10 col-xs-10">
+                        <input
+                          className="form-control"
+                          id="inputPopularity"
+                          type="number"
+                          min="1"
+                          max="5"
+                          ref={(input) => { this.newPopularity = input; }}
+                        />
+                      </div>
+                    </div>
+                    <div className="form-group row">
+                      <label className="col-form-label col-md-2 col-xs-2" htmlFor="inputContent">
+                        Content
+                      </label>
+                      <div className="col-md-10 col-xs-10">
+                        <textarea
+                          className="form-control"
+                          id="inputContent"
+                          rows="10"
+                          ref={(input) => { this.newContent = input; }}
+                          placeholder="Write something..."
+                        />
+                      </div>
+                    </div>
+                  </form>
+                </div>
+              </div>
+              <div className="modal-footer">
+                <button type="button" className="btn btn-secondary" data-dismiss="modal">Close without saving</button>
+                <button type="button" className="btn btn-gourmet" data-dismiss="modal" onClick={this.newArticle}>Save changes</button>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     );
